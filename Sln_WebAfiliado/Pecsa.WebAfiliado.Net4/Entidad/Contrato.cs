e1fa0f4e509fe07c7993aa3a38a890f5b8945696
FunesLisseth <lizfunes1304@gmail.com>
2016-02-13 04:02:47 -0500

    controles nuevos

---
+++ Sln_WebAfiliado/Pecsa.WebAfiliado.Net4/Entidad/Contrato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pecsa.WebAfiliado.Net4.Entidad
+{
+    public class Contrato
+    {
+        public int CodigoContrato { get; set; }
+        public int CodigoResidente { get; set; }
+        public int CodigoVivienda { get; set; }
+        public string CostoMensual { get; set; }
+        public string Estado { get; set; }
+        public string FechaContrato { get; set; }
+        public string FechaCreacion { get; set; }
+        public string FechaIniResidencia { get; set; }
+        public string FechaModificacion { get; set; }
+        public int Periodo { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+    }
+}
---
+++ Sln_WebAfiliado/Pecsa.WebAfiliado.Net4/Entidad/Vivienda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pecsa.WebAfiliado.Net4.Entidad
+{
+    public class Vivienda
+    {
+        public int CodigoVivienda { get; set; }
+
+        public string UbicacionComp { get; set; }
+               
+        public string NumeroVivienda { get; set; }
+
+        public string tipoVivienda { get; set; }
+
+        public decimal Metraje { get; set; }
+
+        public string Caracteristicas { get; set; }
+
+    }
+}
